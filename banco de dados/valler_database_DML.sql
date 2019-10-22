9cc7e2aef2c77d144e96aef51cb9cb311ebf5895
Mateus Santis <mateussantiss@gmail.com>
2019-10-22 14:21:14 -0300

    Att bd

--- banco de dados/valler_database_DML.sql
+++ banco de dados/valler_database_DML.sql
@@ -1,8 +1,9 @@
---Produto - Mateus       - OK
-insert into Produto      (id_categoria, id_usuario, nome, descricao) 
-values                   (1,2,'Fanta', 'Bebida 2L'), 
-                         (1,2,'Coca', 'Bebida 1L'),
-                         (2,4,'Batata', 'Alimento fresco'); 
+use Valler;
+--Tipo_Usuario - Mariana  - OK
+insert into Tipo_Usuario (tipo) 
+values                   ('ADM'),
+                         ('Comum'),
+                         ('Fornecedor');
 
 --Usuario - Mariana      OK
 insert into Usuario      (id_tipo_usuario,nome_razao_social, email,senha,documento) 
@@ -10,18 +11,18 @@ values                   (1,'Valler-adm','adm@valler.com','23751923','2323123421
                          (2,'João Almeida','Joao_almeida@gmail.com','353455344','435625463243'),
                          (3,'EXTRA','extra@extra.com.br','3423453','32434343325');
 
-
---Tipo_Usuario - Mariana  - OK
-insert into Tipo_Usuario (tipo) 
-values                   ('ADM'),
-                         ('Comum'),
-                         ('Fornecedor');
-
 --Categoria - Pietra    -OK
 INSERT INTO Categoria    (categoria)  
 values                   ('Bebidas'),
                          ('Alimentos'),
                          ('Frutas');
+
+
+--Produto - Mateus       - OK
+insert into Produto      (id_categoria, id_usuario, nome_produto, descricao) 
+values                   (1,2,'Fanta', 'Bebida 2L'), 
+                         (1,2,'Coca', 'Bebida 1L'),
+                         (2,3,'Batata', 'Alimento fresco'); 
 
 --Telefone - Pietra      -OK
 INSERT INTO Telefone     (id_usuario,telefone) 
@@ -38,17 +39,21 @@ VALUES                   (1, 'Getulio', 731, 'Jardim Belval', 'Barueri', 'SP', '
 
 --Oferta - Vinicius      -OK
 INSERT INTO Oferta       (id_produto, titulo, data_oferta, data_vencimento, preco, quantidade, imagem)
-VALUES                   (1, 'Prata', '12/09/2019', '20/09/2019', 'R$ 12,00', '1', 'Banana.jpg' ),
-                         (2, 'Ouro', '30/08/2019', '05/09/2019', 'R$14,00' , '2' , 'Banana.jpg') ,
-                         (3, 'Sem Semente', '10/10/2019', '30/10/2019', 'R$6,00', '1', 'Uva.jpg');
+VALUES                   (10, 'Prata', '12/09/2019', '20/09/2019', 'R$ 12,00', '1', 'Banana.jpg' ),
+                         (11, 'Ouro', '30/08/2019', '05/09/2019', 'R$14,00' , '2' , 'Banana.jpg') ,
+                         (12, 'Sem Semente', '10/10/2019', '30/10/2019', 'R$6,00', '1', 'Uva.jpg');
 
 --Reserva - Vinicius    - OK
-INSERT INTO Reserva (id_oferta, id_usuario, quantidade_reserva, cronometro, statuss)
-VALUES                   ('1',  2, '3', '15H', 'aguardando'),
-                         ('2',  2, '2', '14H', 'aguardando'),
-                         ('3',  3, '5', '12H', 'finalisado');
+INSERT INTO Reserva (id_oferta, id_usuario, quantidade_reserva, cronometro, status_reserva)
+VALUES                   (2,  1, 3, '15H', 'aguardando'),
+                         (3,  2, 2, '14H', 'aguardando'),
+                         (4,  3, 5, '12H', 'finalisado');
 
 
+
+
+
+select * from Oferta;
 
 
 
