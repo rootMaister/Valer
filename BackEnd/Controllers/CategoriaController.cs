586f8d92efb9f33ee4ee2823fc43a04c94341351
Mateus Santis <mateussantiss@gmail.com>
2019-10-22 16:16:43 -0300

    Att

---
+++ BackEnd/Controllers/CategoriaController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+
+        VallerContext _context = new VallerContext();
+
+        [HttpGet]
+        public async Task<ActionResult<List<Categoria>>> Get() {
+
+            var categorias = await _context.Categoria.ToListAsync();
+
+            if(categorias == null) {
+                return NotFound();
+            }
+            return categorias;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> Get(int id) {
+
+            var categoria = await _context.Categoria.FindAsync(id);
+
+            if (categoria == null ) {
+                return NotFound();
+            }
+            return categoria;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> Post(Categoria categoria) {
+            
+            try {
+                await _context.Categoria.AddAsync(categoria);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) {
+                throw;
+            }
+            return categoria;
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Categoria categoria){
+
+            if(id != categoria.IdCategoria) {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+
+            try {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) {
+                var categoria_valido = _context.Categoria.FindAsync(id);
+                if(categoria_valido == null) {
+                    return NotFound();
+                }
+                else {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+    }
+}
